00d65d631a6423cda0e1b31527c52ab4e354d11e
Emilian Stankov <eminstine@gmail.com>
2018-02-11 22:52:20 +0200

    Add note tests

--- src/note.rs
+++ src/note.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Note {
     pitch: u8,
     velocity: u8,
@@ -31,5 +32,44 @@ impl Note {
 impl PartialEq for Note {
     fn eq(&self, other: &Note) -> bool {
         self.pitch == other.pitch
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use note::Note;
+    const A4: u8 = 69;
+    #[test]
+    fn test_note() {
+        let note: Note = Note::new(A4, 100);
+        assert_eq!(note.get_pitch(), A4);
+        assert_eq!(note.get_velocity(), 100);
+    }
+
+    #[test]
+    fn test_note_equality() {
+        let note: Note = Note::new(A4, 100);
+        assert_eq!(note, Note::new(A4, 100));
+        assert_eq!(note, Note::new(A4, 10));
+    }
+
+    #[test]
+    fn test_note_inequality() {
+        let note: Note = Note::new(A4, 100);
+        assert_ne!(note, Note::new(70, 100));
+    }
+
+    #[test]
+    fn test_note_mutability() {
+        let mut note: Note = Note::new(A4, 100);
+        note.set_pitch(42);
+        assert_ne!(note, Note::new(A4, 100));
+        assert_eq!(note.get_pitch(), 42);
+        assert_eq!(note, Note::new(42, 100));
+
+        note.set_pitch(A4);
+        note.set_velocity(42);
+        assert_eq!(note.get_velocity(), 42);
+        assert_eq!(note, Note::new(A4, 100));
     }
 }
